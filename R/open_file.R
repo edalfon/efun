32414d890f311bdc55b0e9c3035cec3561d81e82
edalfon <edalfon@gmail.com>
2023-01-08 09:48:31 +0100

    add wait=FALSE for system2 calls

--- R/open_file.R
+++ R/open_file.R
@@ -10,10 +10,10 @@ open_file <- function(file_path) {
   full_path <- shQuote(normalizePath(file_path))
 
   switch(Sys.info()["sysname"], 
-    "Windows" = system2("open", full_path),
-    "Darwin" = system2("open", full_path),
-    "Linux"   = system2("xdg-open", full_path),
-    "SunOS"   = system2("xdg-open", full_path),
-    "FreeBSD" = system2("handlr", paste0("open ", full_path))
+    "Windows" = system2("open", full_path, wait = FALSE),
+    "Darwin" = system2("open", full_path, wait = FALSE),
+    "Linux"   = system2("xdg-open", full_path, wait = FALSE),
+    "SunOS"   = system2("xdg-open", full_path, wait = FALSE),
+    "FreeBSD" = system2("handlr", paste("open", full_path), wait = FALSE)
   )
 }
