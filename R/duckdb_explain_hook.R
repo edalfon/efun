95909493784a65f36ba5f23f05160a85c943c917
edalfon <edalfon@gmail.com>
2024-04-25 19:32:52 +0200

    fix: update .Rbuildignore

--- R/duckdb_explain_hook.R
+++ R/duckdb_explain_hook.R
@@ -20,8 +20,7 @@
 #' # you want to override standard printing of sql chunks
 #' efun::duckdb_explain_hook()
 #' }
-duckdb_explain_hook <- function (all = NULL)
-{
+duckdb_explain_hook <- function(all = NULL) {
   # Add a custom chunk hook, as documented here:
   # https://bookdown.org/yihui/rmarkdown-cookbook/chunk-hooks.html
   # Which basically allows you to execute code before and after
@@ -29,29 +28,26 @@ duckdb_explain_hook <- function (all = NULL)
   # So let's call the chunk option `duckdb_explain`
   knitr::knit_hooks$set(duckdb_explain = function(before) {
     if (before) {
-      #' Now, before running the chunk we want to sets a custom
-      #' printing function for SQL code chunks. It overrides the
-      #' default behavior for rendering SQL code, by setting the
-      #' `sql.print` option of knitr. When you set this, knitr
-      #' will call that function passing the data that results from
-      #' the query and then print the result using cat. Note also
-      #' that we wrap this in code display tags, because when you
-      #' use the sql.print option, knitr will force the results='asis'.
-      #' See line 653 of the sql knitr engine.
-      #' https://github.com/yihui/knitr/blob/master/R/engine.R
-      #' if (!is.null(sql.print)) {
-      #'   options$results = 'asis'
-      #'   cat(sql.print(data))
-      #' }
+      # Now, before running the chunk we want to sets a custom
+      # printing function for SQL code chunks. It overrides the
+      # default behavior for rendering SQL code, by setting the
+      # `sql.print` option of knitr. When you set this, knitr
+      # will call that function passing the data that results from
+      # the query and then print the result using cat. Note also
+      # that we wrap this in code display tags, because when you
+      # use the sql.print option, knitr will force the results='asis'.
+      # See line 653 of the sql knitr engine.
+      # https://github.com/yihui/knitr/blob/master/R/engine.R
+      # if (!is.null(sql.print)) {
+      #   options$results = 'asis'
+      #   cat(sql.print(data))
+      # }
       knitr::opts_knit$set(sql.print = function(x) {
         paste0("<pre><code>", x$explain_value, "</code></pre>")
       })
-    }
-    else {
+    } else {
       knitr::opts_knit$set(sql.print = NULL)
     }
   })
   knitr::opts_chunk$set(duckdb_explain = all)
 }
-
-
