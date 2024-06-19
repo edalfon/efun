7101c9b93a5f442828d992a82d684d1b295f68c9
edalfon <edalfon@gmail.com>
2024-06-19 04:36:24 +0200

    tests: add test to reproduce the error in assert working on grouped data

--- tests/testthat/test-assert.R
+++ tests/testthat/test-assert.R
@@ -3,3 +3,10 @@ test_that("assert works", {
 
   expect_identical(mtcars |> assert(mpg < 34), mtcars)
 })
+
+test_that("assert does not fail on grouped data frames", {
+  expect_error(
+    object = mtcars |> dplyr::group_by(cyl) |> assert(hp < 150),
+    regexp = "Assertion does not hold"
+  )
+})
