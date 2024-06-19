2db90489787bcc8e1489b038b7c60a372df36b77
edalfon <edalfon@gmail.com>
2024-06-19 18:05:11 +0200

    tests: add tests for multiple conditions and hanlding NAs

--- tests/testthat/test-assert.R
+++ tests/testthat/test-assert.R
@@ -1,12 +1,42 @@
-test_that("assert works", {
+test_that("assert() works", {
   expect_error(mtcars |> assert(mpg < 33))
 
   expect_identical(mtcars |> assert(mpg < 34), mtcars)
 })
 
-test_that("assert does not fail on grouped data frames", {
+test_that("assert() does not fail on grouped data frames", {
   expect_error(
     object = mtcars |> dplyr::group_by(cyl) |> assert(hp < 150),
     regexp = "Assertion does not hold"
+  )
+})
+
+test_that("assert() does not fail testing data with NAs", {
+  expect_no_error(
+    object = mtcars |>
+      dplyr::mutate(mpg = dplyr::na_if(mpg, 15)) |>
+      assert(mpg > 10)
+  )
+})
+
+test_that("assert() can handle multiple conditions", {
+  expect_no_error(
+    object = mtcars |>
+      dplyr::mutate(mpg = dplyr::na_if(mpg, 15)) |>
+      assert(hp < 1500, am < 100)
+  )
+
+  expect_error(
+    object = mtcars |>
+      dplyr::mutate(mpg = dplyr::na_if(mpg, 15)) |>
+      assert(hp < 1500, am < 1),
+    regexp = "Assertion does not hold.*am < 1.*"
+  )
+
+  expect_error(
+    object = mtcars |>
+      dplyr::mutate(mpg = dplyr::na_if(mpg, 15)) |>
+      assert(hp < 1, am < 100),
+    regexp = "Assertion does not hold.*hp < 1.*"
   )
 })
